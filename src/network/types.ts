046b25cf1adfb08d59ad520267b4091284b7fa2b
ElshadHu <elsadhumbetli079@gmail.com>
2025-11-11 00:15:44 -0500

    Initial types

---
+++ src/network/types.ts
@@ -0,0 +1,68 @@
+export enum SocketState {
+    CREATED,
+    BOUND,
+    LISTENING,
+    CONNECTED,
+    CLOSING,
+    CLOSED,
+    ERROR
+}
+
+
+export type Address = {
+    host: string;  // localhost 
+    port: number; //8080
+}
+
+export type RemoteAddress = {
+    id: string; // client's IP
+    port: number; // client's port
+}
+
+// Config types
+
+export type ListenerConfig = {
+    port: number;
+    host: string;
+    backlog?: number // max pending connections
+}
+
+// Info Types
+
+export type ConnectionInfo = {
+    id: string;
+    remoteAddress: RemoteAddress;
+    connectedAt: Date;
+    state: SocketState;
+}
+
+// Callback types
+
+export type ConnectionCallBack = (connection: IConnection) => void | Promise<void>;
+export type DataCallback = (data: any) => void;
+export type CloseCallback = () => void;
+export type ErrorCallBack = (error: Error) => void;
+
+
+export interface IListener {
+    bind(port: number, host: string): void;
+    listen(backlog?: number): void;
+    onConnection(callback: ConnectionCallBack): void;
+    close():void;
+    destroy():void;
+    getState(): SocketState;
+    getAddress(): Address;
+}
+
+export interface IConnection {
+    write(data: Buffer | string): boolean;
+    onData(callback: DataCallback): void;
+    onClose(callback: CloseCallback): void;
+    onError(callback:ErrorCallBack): void;
+    close(): void;
+    destroy(): void;
+    getRemoteAddress(): RemoteAddress;
+    getState(): SocketState;
+    getId(): string;
+
+}
