61d9ba919fc44a7560c8964963d26b2936f7911f
ElshadHu <elsadhumbetli079@gmail.com>
2025-11-26 21:08:17 -0500

    Add routes

---
+++ src/routes/index.ts
@@ -0,0 +1,48 @@
+import { HttpRequest } from "../http/models/request";
+import { HttpResponse } from "../http/models/response";
+import { HttpStatusCode } from "../http/models/StatusCode";
+import { HttpServer } from "../server/httpServer";
+
+export function registerRoutes(server: HttpServer): void {
+    // GET
+    server.route('GET','/', (req:HttpRequest, res:HttpResponse)=> {
+        res.setHtmlBody('<h1>HTTP Server</h1><p>Server is running</p>');
+    });
+    // GET /status
+    server.route('GET','/status', (req: HttpRequest, res:HttpResponse) => {
+        res.setJsonBody({
+            status: 'running',
+            uptime: process.uptime(),
+            memory: process.memoryUsage(),
+            timestamp: new Date().toISOString(),
+            pid: process.pid
+        });
+    });
+
+    server.route('POST', '/api/users', (req: HttpRequest, res: HttpResponse) => {
+       if(!req.parsedBody) {
+         res.setStatus(HttpStatusCode.BAD_REQUEST);
+         res.setJsonBody({error: 'No body provided'});
+         return;
+       }
+       const name = req.parsedBody.name;
+       const age = req.parsedBody.age;
+
+       const ageNum = typeof age === 'string' 
+       ? parseInt(age,10)
+       : typeof age === 'number'
+       ? age
+       : NaN
+
+       if( typeof name !== 'string' || isNaN(ageNum)) {
+         res.setStatus(HttpStatusCode.BAD_REQUEST);
+         res.setJsonBody({error: 'Invalid data types'});
+         return;
+       }
+       res.setStatus(HttpStatusCode.CREATED);
+       res.setJsonBody({
+         message: 'User created',
+         user: {name,age: ageNum}
+       });
+    });
+}
